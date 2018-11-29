e613a210e2c95d84846fd1987782a7477a051f6f
Sphinx <sphinx010f1@gmail.com>
2018-11-29 15:16:31 +0200

    work commit

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,7 +6,18 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int a;
+            Console.WriteLine("Введите целое число a");
+            while (!int.TryParse(Console.ReadLine(), out a))
+            {
+                Console.WriteLine("Ошибка ввода! Введите целое число a");
+            }
+
+            int[] arr = new[] {Int32.Parse(Console.ReadLine()), 5, a};
+            foreach (var i in arr)
+            {
+                Console.Write(i + ", ");
+            }
         }
     }
 }
